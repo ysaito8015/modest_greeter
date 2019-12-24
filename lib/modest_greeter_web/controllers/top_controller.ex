0bec2bb2d744aec2e79274818a83210b29808ed4
Yusuke_Saito <ysaito8015@gmail.com>
2019-12-24 06:24:14 +0100

    done 16.2 top0

--- lib/modest_greeter_web/controllers/top_controller.ex
+++ lib/modest_greeter_web/controllers/top_controller.ex
@@ -2,6 +2,6 @@ defmodule ModestGreeterWeb.TopController do
   use ModestGreeterWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html", name: "Top"
+    render conn, "index.html"
   end
 end
--- lib/modest_greeter_web/views/top_view.ex
+++ lib/modest_greeter_web/views/top_view.ex
@@ -1,3 +1,9 @@
 defmodule ModestGreeterWeb.TopView do
   use ModestGreeterWeb, :view
+
+  def document_title(_assigns) do
+    "ModestGreeter"
+  end
 end
+
+
